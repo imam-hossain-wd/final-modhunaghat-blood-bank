7b319995493ddd52e10cc9c6b7b6fb37d1cc579f
imam-hossain-wd <zunayedemon6t9@gmail.com>
2024-10-02 15:31:44 +0600

    blood donor feacture causes and get involve section added

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,4 +1,7 @@
+import BloodDonor from "@/components/main/Home/BloodDonor/BloodDonor";
+import FeaturedCauses from "@/components/main/Home/FeactureCauses/FeactureCauses";
 import FindBlood from "@/components/main/Home/FindBlood/FindBlood";
+import GetInvolved from "@/components/main/Home/Getinvolve/Getinvolve";
 import HomeBanner from "@/components/main/Home/HomeBanner/HomeBanner";
 
 const HomePage = () => {
@@ -6,6 +9,9 @@ const HomePage = () => {
         <div>
            <HomeBanner />
            <FindBlood />
+           <BloodDonor />
+           <FeaturedCauses />
+           <GetInvolved />
         </div>
     );
 };
